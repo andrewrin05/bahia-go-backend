1199756a46c35cdb424cc5e694102db659c4a12a
andrewrin05 <arincon134@alumno.uned.es>
2026-02-12 11:23:28 +0100

    add: simple app controller to test backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ListingsModule } from './listings/listings.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -11,5 +12,6 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     ListingsModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
